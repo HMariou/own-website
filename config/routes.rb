3f537dbc61afff33dae2986be62a48f9648e8ded
hugo mariou <mariou.hugo@gmail.com>
2023-02-27 05:58:25 -0500

    restored version of my_website

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root to: "pages#landing"
+  get "about", to: "pages#about"
+  get "contact", to: "pages#contact"
+  get "project", to: "pages#project"
   # Defines the root path route ("/")
   # root "articles#index"
 end
